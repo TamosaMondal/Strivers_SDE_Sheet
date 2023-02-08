31b95b53d2115a9d452b954df07a96ac098c9668
Tamosa <77878238+TamosaMondal@users.noreply.github.com>
2023-02-08 19:52:08 +0530

    Optimized & Easy approach💥📢

---
+++ pow(x, n).java
@@ -0,0 +1,22 @@
+/*Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+*/
+
+class Solution {
+    public double myPow(double x, int n) {
+        double ans=1.0;
+        long nn=n;
+        if(nn<0) nn= -1 * nn;
+        while(nn>0){
+            if(nn%2==1){
+                ans=ans*x;
+                nn=nn-1;
+            }
+            else{
+                x=x*x;
+                nn=nn/2;
+            }
+        }
+        if(n<0) ans=(double)(1.0)/(double)(ans);    //if it is negetive
+        return ans;
+    }
+}
