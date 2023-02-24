3ee142429495fad0cb913d41f29b2fcb88bdf03a
Tamosa <77878238+TamosaMondal@users.noreply.github.com>
2023-02-24 21:44:44 +0530

    📢🔥Optimized Solution||2ms||Easy

---
+++ combination Sum II.java
@@ -0,0 +1,30 @@
+/*Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note: The solution set must not contain duplicate combinations.
+*/
+
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(candidates);
+        findCombinations(0,candidates,target,ans,new ArrayList());
+        return ans;
+    }
+    private void findCombinations(int ind, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds) {
+        if(target == 0) {
+            ans.add(new ArrayList<>(ds)); 
+            return; 
+        }
+        
+        for(int i = ind; i < arr.length;i++) {
+            if(i > ind && arr[i] == arr[i-1]) continue; 
+            if(arr[i]>target) break; 
+            
+            ds.add(arr[i]); 
+            findCombinations(i+1, arr, target - arr[i], ans, ds); 
+            ds.remove(ds.size() - 1); 
+        }
+    }
+}
