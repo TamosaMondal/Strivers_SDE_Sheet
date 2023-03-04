d8fefc72872a8214def0bf3c7bd5c06c427c28ce
Tamosa <77878238+TamosaMondal@users.noreply.github.com>
2023-03-04 15:50:47 +0530

    🧨💥1ms||Easy to UnderStand

---
+++ Maximum Width of Binary Tree.java
@@ -0,0 +1,67 @@
+/*Given the root of a binary tree, return the maximum width of the given tree.
+
+The maximum width of a tree is the maximum width among all levels.
+
+The width of one level is defined as the length between the end-nodes (the leftmost and rightmost non-null nodes), where the null nodes between the end-nodes that would be present in a complete binary tree extending down to that level are also counted into the length calculation.
+
+It is guaranteed that the answer will in the range of a 32-bit signed integer.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ class TreeNode {
+  int data;
+  TreeNode  left,  right;
+  TreeNode(int data){
+      this.data=data;
+      left=null;
+      right=null;
+  }
+}
+class Pair {
+    TreeNode node; 
+    int num; 
+    Pair(TreeNode _node, int _num) {
+        num = _num;
+        node = _node; 
+    }
+}
+class Solution {
+    public int widthOfBinaryTree(TreeNode root) {
+        if(root == null) return 0;
+        int maxWidth = 0;
+        Queue<Pair> q = new LinkedList<>(); 
+        q.offer(new Pair(root, 0)); 
+        while(!q.isEmpty()){
+            int size = q.size();
+            int mmin = q.peek().num;    //to make the id starting from zero
+            int first = 0,last = 0;
+            for(int i=0; i<size; i++){
+                int cur_id = q.peek().num-mmin;
+                TreeNode node = q.peek().node;
+                q.poll();
+                if(i==0) first = cur_id;
+                if(i==size-1) last = cur_id;
+                if(node.left != null)
+                    q.offer(new Pair(node.left, cur_id*2+1));
+                if(node.right != null) 
+                    q.offer(new Pair(node.right, cur_id*2+2));
+            }
+            maxWidth = Math.max(maxWidth, last-first+1);
+        }
+        return maxWidth;
+    }
+}
