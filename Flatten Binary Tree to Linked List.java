9356f74eb6db30b09511055e00dcab2123c02717
Tamosa <77878238+TamosaMondal@users.noreply.github.com>
2023-03-11 10:51:42 +0530

    🚨💥Recursive||Easy||0ms

---
+++ Flatten Binary Tree to Linked List.java
@@ -0,0 +1,32 @@
+/*Given the root of a binary tree, flatten the tree into a "linked list":
+
+The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    TreeNode prev=null;
+    public void flatten(TreeNode root) {
+        if(root==null) return;
+        flatten(root.right);
+        flatten(root.left);
+        root.right=prev;
+        root.left=null;
+        prev=root;
+    }
+}
